b1f0f269aedf4e31718bca2af838ec7a83ca0c75
iqbal22pcr <m.iqbal22ti@mahasiswa.pcr.ac.id>
2023-12-13 21:54:11 +0700

    Update beranda.php
    Nambahin milestone Progress Donasi, Masih Static

--- application/views/User/beranda.php
+++ application/views/User/beranda.php
@@ -1,137 +1,148 @@
-    <!--Overlay-->
-        <div class="bg-cover mx-12 my-5 m-auto max-w-9xl bg-no-repeat h-[600px] 
+<!--Overlay-->
+<div class="bg-cover mx-12 my-5 m-auto max-w-9xl bg-no-repeat h-[600px] 
         relative rounded-md" style="background-image: url(./assets/img/2.jpg);">
-            <div class="absolute inset-0 bg-black opacity-50 rounded-md shadow-black shadow-md"></div>
-            <div class="flex-col">
-                <div class="absolute inset-0 flex items-center justify-center text-white text-5xl font-bold">
-                    Welcome to <div class="ml-2 text-yellow-100">Volun<span class="text-yellow-500">T</span>rees</div>
-                </div>
-                <div class="absolute mt-24 inset-0 flex items-center justify-center text-white text-3xl font-semibold">
-                    "Bersama sama Menjaga Hutan"
-                </div>
-
-            </div>
+    <div class="absolute inset-0 bg-black opacity-50 rounded-md shadow-black shadow-md"></div>
+    <div class="flex-col">
+        <div class="absolute inset-0 flex items-center justify-center text-white text-5xl font-bold">
+            Welcome to <div class="ml-2 text-yellow-100">Volun<span class="text-yellow-500">T</span>rees</div>
+        </div>
+        <div class="absolute mt-24 inset-0 flex items-center justify-center text-white text-3xl font-semibold">
+            "Bersama sama Menjaga Hutan"
         </div>
 
-        <!-- Penghijauan Voluntrees -->
-        <div class="py-20 m-auto">
-            <h1 class="font-medium font-sans text-center text-yellow-50 text-[48px]">Penghijauan Voluntrees</h1>
-            <div class="flex items-center text-center justify-center space-x-28 mt-20 text-9xl text-yellow-50">
-                <div>
-                    <div class="text-yellow-900">2.5jt</div>
-                    <p class="text-3xl py-8">donasi terkumpul</p>
-                </div>
-                <div>
-                    <div class="text-yellow-900">2k</div>
-                    <p class="text-3xl py-8">Pohon Tertanam</p>
-                </div>
-                <div>
-                    <div class="text-yellow-900">10</div>
-                    <p class="text-3xl py-8">Event Terselesaikan</p>
-                </div>
-            </div>
+    </div>
+</div>
+
+<!-- Penghijauan Voluntrees -->
+<div class="py-20 m-auto">
+    <h1 class="font-medium font-sans text-center text-yellow-50 text-[48px]">Penghijauan Voluntrees</h1>
+    <div class="flex items-center text-center justify-center space-x-28 mt-20 text-9xl text-yellow-50">
+        <div>
+            <div class="text-yellow-900">2.5jt</div>
+            <p class="text-3xl py-8">donasi terkumpul</p>
         </div>
-
-        <!-- Donasi -->
-        <div class="py-20 m-auto">
-            <h1 class="font-medium font-sans text-center text-yellow-50 text-[48px]">Target Donasi</h1>
-            <div class="-mb-14 flex justify-end mt-12 mr-28">
-                <button type="submit" class="bg-white hover:bg-[#d4d4d4] text-[#496b4a] font-bold py-2 px-4 rounded">
-                    Lihat Selengkapnya
-                </button>
-            </div>
-            <div class="flex justify-center mt-16">
-                <div class="p-4 m-8 w-80 bg-white shadow-md rounded-xl">
-                    <picture class="rounded-lg block overflow-hidden">
-                        <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="./assets/img/Fire1.jpg"
-                            alt="Pekanbaru" />
-                    </picture>
-
-                    <h1 class="mt-4 mb-2 text-xl font-bold">Pekanbaru</h1>
-                    <a href="<?= base_url('User/formDonasi'); ?>" class="bg-[#527853] hover:bg-[#496b4a] text-white font-bold py-2 px-4 rounded">Donasi Sekarang</a>
-                </div>
-                <div class="p-4 m-8 w-80 bg-white shadow-md rounded-xl">
-                    <picture class="rounded-lg block overflow-hidden">
-                        <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="./assets/img/Fire2.jpg"
-                            alt="Bengkalis" />
-                    </picture>
-                    <h1 class="mt-4 mb-2 text-xl font-bold">Bengkalis</h1>
-                    <a href="<?= base_url('User/formDonasi'); ?>" class="bg-[#527853] hover:bg-[#496b4a] text-white font-bold py-2 px-4 rounded">Donasi Sekarang</a>
-                </div>
-                <div class="p-4 m-8 w-80 bg-white shadow-md rounded-xl">
-                    <picture class="rounded-lg block overflow-hidden">
-                        <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="./assets/img/Fire3.jpg"
-                            alt="Bengkalis" />
-                    </picture>
-                    <h1 class="mt-4 mb-2 text-xl font-bold">Kerinci</h1>
-                    <a href="<?= base_url('User/formDonasi'); ?>" class="bg-[#527853] hover:bg-[#496b4a] text-white font-bold py-2 px-4 rounded">Donasi Sekarang</a>
-                </div>
-                <div class="p-4 m-8 w-80 bg-white shadow-md rounded-xl">
-                    <picture class="rounded-lg block overflow-hidden">
-                        <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="./assets/img/Fire4.jpg"
-                            alt="Bengkalis" />
-                    </picture>
-                    <h1 class="mt-4 mb-2 text-xl font-bold">Dumai</h1>
-                    <a href="<?= base_url('User/formDonasi'); ?>" class="bg-[#527853] hover:bg-[#496b4a] text-white font-bold py-2 px-4 rounded">Donasi Sekarang</a>
-                </div>
-            </div>
+        <div>
+            <div class="text-yellow-900">2k</div>
+            <p class="text-3xl py-8">Pohon Tertanam</p>
         </div>
-
-        <!-- Event-->
-        <div class="py-20 m-auto pb-40">
-            <h1 class="font-medium font-sans text-center text-yellow-50 text-[48px]">Event Voluntrees</h1>
-            <div class="-mb-14 flex justify-end mt-12 mr-28">
-                <button type="submit" class="bg-white hover:bg-[#d4d4d4] text-[#496b4a] font-bold py-2 px-4 rounded">
-                    Lihat Selengkapnya
-                </button>
-            </div>
-            <div class="flex justify-center mt-16">
-                <div class="p-4 m-8 w-80 bg-white shadow-md rounded-xl">
-                    <picture class="rounded-lg block overflow-hidden">
-                        <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="./assets/img/Fire1.jpg"
-                            alt="Pekanbaru" />
-                    </picture>
-
-                    <h1 class="mt-4 mb-2 text-xl font-bold">Penanaman-Pekanbaru</h1>
-                    <button type="submit"
-                        class="bg-[#527853] hover:bg-[#496b4a] text-white font-bold py-2 px-4 rounded">
-                        Daftar Sekarang
-                    </button>
-                </div>
-                <div class="p-4 m-8 w-80 bg-white shadow-md rounded-xl">
-                    <picture class="rounded-lg block overflow-hidden">
-                        <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="./assets/img/Fire2.jpg"
-                            alt="Bengkalis" />
-                    </picture>
-                    <h1 class="mt-4 mb-2 text-xl font-bold">Penanaman-Bengkalis</h1>
-                    <button type="submit"
-                        class="bg-[#527853] hover:bg-[#496b4a] text-white font-bold py-2 px-4 rounded">
-                        Daftar Sekarang
-                    </button>
-                </div>
-                <div class="p-4 m-8 w-80 bg-white shadow-md rounded-xl">
-                    <picture class="rounded-lg block overflow-hidden">
-                        <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="./assets/img/Fire3.jpg"
-                            alt="Bengkalis" />
-                    </picture>
-
-                    <h1 class="mt-4 mb-2 text-xl font-bold">Penanaman-Kerinci</h1>
-                    <button type="submit"
-                        class="bg-[#527853] hover:bg-[#496b4a] text-white font-bold py-2 px-4 rounded">
-                        Daftar Sekarang
-                    </button>
-                </div>
-                <div class="p-4 m-8 w-80 bg-white shadow-md rounded-xl">
-                    <picture class="rounded-lg block overflow-hidden">
-                        <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="./assets/img/Fire4.jpg"
-                            alt="Bengkalis" />
-                    </picture>
-
-                    <h1 class="mt-4 mb-2 text-xl font-bold">Penanaman-Dumai</h1>
-                    <button type="submit"
-                        class="bg-[#527853] hover:bg-[#496b4a] text-white font-bold py-2 px-4 rounded">
-                        Daftar Sekarang
-                    </button>
-                </div>
-            </div>
+        <div>
+            <div class="text-yellow-900">10</div>
+            <p class="text-3xl py-8">Event Terselesaikan</p>
         </div>
+    </div>
+</div>
+
+<!-- Donasi -->
+<div class="py-20 m-auto">
+    <h1 class="font-medium font-sans text-center text-yellow-50 text-[48px]">Target Donasi</h1>
+    <div class="-mb-14 flex justify-end mt-12 mr-28">
+        <button type="submit" class="bg-white hover:bg-[#d4d4d4] text-[#496b4a] font-bold py-2 px-4 rounded">
+            Lihat Selengkapnya
+        </button>
+    </div>
+    <div class="flex justify-center mt-16">
+        <div class="p-4 m-8 w-80 bg-white shadow-md rounded-xl">
+            <picture class="rounded-lg block overflow-hidden">
+                <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="./assets/img/Fire1.jpg"
+                    alt="Pekanbaru" />
+            </picture>
+            <h1 class="mt-4 mb-2 text-xl font-bold">Pekanbaru</h1>
+            <div class="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700">
+                <div class="bg-yellow-400 h-2.5 rounded-full dark:bg-gray-300" style="width: 45%"></div>
+            </div>
+            <a href="<?= base_url('User/formDonasi'); ?>"
+                class="bg-[#527853] hover:bg-[#496b4a] text-white font-bold py-2 px-4 rounded">Donasi Sekarang</a>
+        </div>
+        <div class="p-4 m-8 w-80 bg-white shadow-md rounded-xl">
+            <picture class="rounded-lg block overflow-hidden">
+                <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="./assets/img/Fire2.jpg"
+                    alt="Bengkalis" />
+            </picture>
+            <h1 class="mt-4 mb-2 text-xl font-bold">Bengkalis</h1>
+            <div class="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700">
+                <div class="bg-yellow-400 h-2.5 rounded-full dark:bg-gray-300" style="width: 15%"></div>
+            </div>
+            <a href="<?= base_url('User/formDonasi'); ?>"
+                class="bg-[#527853] hover:bg-[#496b4a] text-white font-bold py-2 px-4 rounded">Donasi Sekarang</a>
+        </div>
+        <div class="p-4 m-8 w-80 bg-white shadow-md rounded-xl">
+            <picture class="rounded-lg block overflow-hidden">
+                <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="./assets/img/Fire3.jpg"
+                    alt="Bengkalis" />
+            </picture>
+            <h1 class="mt-4 mb-2 text-xl font-bold">Kerinci</h1>
+            <div class="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700">
+                <div class="bg-yellow-400 h-2.5 rounded-full dark:bg-gray-300" style="width: 70%"></div>
+            </div>
+            <a href="<?= base_url('User/formDonasi'); ?>"
+                class="bg-[#527853] hover:bg-[#496b4a] text-white font-bold py-2 px-4 rounded">Donasi Sekarang</a>
+        </div>
+        <div class="p-4 m-8 w-80 bg-white shadow-md rounded-xl">
+            <picture class="rounded-lg block overflow-hidden">
+                <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="./assets/img/Fire4.jpg"
+                    alt="Bengkalis" />
+            </picture>
+            <h1 class="mt-4 mb-2 text-xl font-bold">Dumai</h1>
+            <div class="w-full bg-gray-200 rounded-full h-2.5 mb-4 dark:bg-gray-700">
+                <div class="bg-yellow-400 h-2.5 rounded-full dark:bg-gray-300" style="width: 90%"></div>
+            </div>
+            <a href="<?= base_url('User/formDonasi'); ?>"
+                class="bg-[#527853] hover:bg-[#496b4a] text-white font-bold py-2 px-4 rounded">Donasi Sekarang</a>
+        </div>
+    </div>
+</div>
+
+<!-- Event-->
+<div class="py-20 m-auto pb-40">
+    <h1 class="font-medium font-sans text-center text-yellow-50 text-[48px]">Event Voluntrees</h1>
+    <div class="-mb-14 flex justify-end mt-12 mr-28">
+        <button type="submit" class="bg-white hover:bg-[#d4d4d4] text-[#496b4a] font-bold py-2 px-4 rounded">
+            Lihat Selengkapnya
+        </button>
+    </div>
+    <div class="flex justify-center mt-16">
+        <div class="p-4 m-8 w-80 bg-white shadow-md rounded-xl">
+            <picture class="rounded-lg block overflow-hidden">
+                <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="./assets/img/Fire1.jpg"
+                    alt="Pekanbaru" />
+            </picture>
+
+            <h1 class="mt-4 mb-2 text-xl font-bold">Penanaman-Pekanbaru</h1>
+            <button type="submit" class="bg-[#527853] hover:bg-[#496b4a] text-white font-bold py-2 px-4 rounded">
+                Daftar Sekarang
+            </button>
+        </div>
+        <div class="p-4 m-8 w-80 bg-white shadow-md rounded-xl">
+            <picture class="rounded-lg block overflow-hidden">
+                <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="./assets/img/Fire2.jpg"
+                    alt="Bengkalis" />
+            </picture>
+            <h1 class="mt-4 mb-2 text-xl font-bold">Penanaman-Bengkalis</h1>
+            <button type="submit" class="bg-[#527853] hover:bg-[#496b4a] text-white font-bold py-2 px-4 rounded">
+                Daftar Sekarang
+            </button>
+        </div>
+        <div class="p-4 m-8 w-80 bg-white shadow-md rounded-xl">
+            <picture class="rounded-lg block overflow-hidden">
+                <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="./assets/img/Fire3.jpg"
+                    alt="Bengkalis" />
+            </picture>
+
+            <h1 class="mt-4 mb-2 text-xl font-bold">Penanaman-Kerinci</h1>
+            <button type="submit" class="bg-[#527853] hover:bg-[#496b4a] text-white font-bold py-2 px-4 rounded">
+                Daftar Sekarang
+            </button>
+        </div>
+        <div class="p-4 m-8 w-80 bg-white shadow-md rounded-xl">
+            <picture class="rounded-lg block overflow-hidden">
+                <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="./assets/img/Fire4.jpg"
+                    alt="Bengkalis" />
+            </picture>
+
+            <h1 class="mt-4 mb-2 text-xl font-bold">Penanaman-Dumai</h1>
+            <button type="submit" class="bg-[#527853] hover:bg-[#496b4a] text-white font-bold py-2 px-4 rounded">
+                Daftar Sekarang
+            </button>
+        </div>
+    </div>
+</div>
