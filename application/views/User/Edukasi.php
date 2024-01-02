5b4317d088163b87282e4441063a37b1e1a67c61
josuaewaldo <josua22ti@mahasiswa.pcr.ac.id>
2024-01-02 12:01:35 +0700

    Edukasi dan News

---
+++ application/views/User/Edukasi.php
@@ -0,0 +1,37 @@
+<!--Overlay-->
+<div class="bg-cover mx-12 my-5 m-auto max-w-9xl bg-no-repeat h-[600px] 
+        relative rounded-md" style="background-image: url('<?php echo base_url("assets/img/"); ?>News1.jpg');">
+    <div class="absolute inset-0 bg-black opacity-50 rounded-md shadow-black shadow-md"></div>
+    <div class="flex-col">
+        <div class="absolute inset-0 flex items-center justify-center text-white text-5xl font-bold">
+          <b> SELAMAT DATANG DI VOLUNEDUCATION</b>
+        </div>
+        <div class="absolute mt-24 inset-0 flex items-center justify-center text-white text-3xl font-semibold">
+          <i> Informasi mengenai apa saja dalam penanaman pohon </i>
+        </div>
+
+    </div>
+</div>
+
+<!-- Penghijauan Voluntrees -->
+<div class="py-20 m-auto">
+    <b><h1 class="font-semibold text-center text-yellow-50 text-[48px] font-poppins">Edukasi</h1></b>
+    <div class="flex items-start text-center justify-center space-x-28 mt-20 text-9xl text-yellow-50 font-poppins">
+        <div>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/y5ITW3Zz4yc?si=f3V3Nds6gtmNE-JT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+        <div>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/71-B0DV-c8E?si=VRqd5E7GPUoxWJXu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+    </div>
+<div class="py-20 m-auto">
+    <div class="flex items-start text-center justify-center space-x-28 mt-20 text-9xl text-yellow-50 font-poppins">
+        <div>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/cr2TI64yRI0?si=5Dpg4VAhUeBVVvGh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+        <div>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/WvmaRWRarSE?si=4fci0sFqYn0cyGm3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+    </div>
+</div>
+</div>
