b8a67f4aa7fda2ecaf29aec2b7a80c5fab774ec3
josuaewaldo <josua22ti@mahasiswa.pcr.ac.id>
2024-01-02 12:49:18 +0700

    Reward

---
+++ application/views/User/Reward.php
@@ -0,0 +1,70 @@
+<!--Overlay-->
+<div class="bg-cover mx-12 my-5 m-auto max-w-9xl bg-no-repeat h-[600px] 
+        relative rounded-md" style="background-image: url('<?php echo base_url("assets/img/"); ?>News1.jpg');">
+    <div class="absolute inset-0 bg-black opacity-50 rounded-md shadow-black shadow-md"></div>
+    <div class="flex-col">
+        <div class="absolute inset-0 flex items-center justify-center text-white text-5xl font-bold">
+          <b> SELAMAT DATANG DI VOLUNWARD</b>
+        </div>
+        <div class="absolute mt-24 inset-0 flex items-center justify-center text-white text-3xl font-semibold">
+          <i> Inilah yang dapat kami kasih untuk kalian para VolunTrees Tercinta</i>
+        </div>
+
+    </div>
+</div>
+
+<!-- Penghijauan Voluntrees -->
+<div class="py-20 m-auto">
+    <b><h1 class="font-semibold text-center text-yellow-50 text-[48px] font-poppins">Reward</h1></b>
+    <div class="flex items-start text-center justify-center space-x-28 mt-20 text-9xl text-yellow-50 font-poppins">
+        <div>
+        <picture class="rounded-lg block overflow-hidden">
+                <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="<?= base_url('assets/img/'); ?>Reward1.jpeg"
+                     />
+            </picture> 
+           <b> <div class="text-primary-yellow-100"><font size="4">Voucher Steam Wallter 100K</font></div></b>
+           <a href="<?= base_url('User/REEDEM3'); ?>"><p class="text-3xl py-8">REEDEM</p></a>
+        </div>
+        
+        <div>
+        <picture class="rounded-lg block overflow-hidden">
+                <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="<?= base_url('assets/img/'); ?>Reward2.png"
+                     />
+                     <b> <div class="text-primary-yellow-100"><font size="4">Voucher Indomaret 50K</font></div></b>
+                     <a href="<?= base_url('User/REEDEM3'); ?>"><p class="text-3xl py-8">REEDEM</p></a>
+        </div>
+        <div>
+        <picture class="rounded-lg block overflow-hidden">
+                <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="<?= base_url('assets/img/'); ?>Reward3.png"
+                     />
+                     <b> <div class="text-primary-yellow-100"><font size="4">Voucher Alfamart 50K</font></div></b>
+                     <a href="<?= base_url('User/REEDEM3'); ?>"><p class="text-3xl py-8">REEDEM</p></a>
+        </div>
+        </div>
+<div class="py-20 m-auto">
+<div class="flex items-start text-center justify-center space-x-28 mt-20 text-9xl text-yellow-50 font-poppins">
+        <div>
+        <picture class="rounded-lg block overflow-hidden">
+                <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="<?= base_url('assets/img/'); ?>Reward4.png"
+                     />
+            <b> <div class="text-primary-yellow-100"><font size="4">Voucher Janji Jiwa Gratis 1 Minum</font></div></b>
+            <a href="<?= base_url('User/REEDEM3'); ?>"><p class="text-3xl py-8">REEDEM</p></a>
+        </div>
+        <div>
+        <picture class="rounded-lg block overflow-hidden">
+                <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="<?= base_url('assets/img/'); ?>Reward5.jpg"
+                     />
+            <b> <div class="text-primary-yellow-100"><font size="4">Voucher KKV 10K</font></div></b>
+            <a href="<?= base_url('User/REEDEM3'); ?>"><p class="text-3xl py-8">REEDEM</p></a>
+        </div>
+        <div>
+        <picture class="rounded-lg block overflow-hidden">
+                <img class="w-full h-40 object-cover hover:scale-125 ease-in duration-150" src="<?= base_url('assets/img/'); ?>Reward6.jpg"
+                     />
+            <b> <div class="text-primary-yellow-100"><font size="4">Voucher Farmers Market 40K</font></div></b>
+            <a href="<?= base_url('User/REEDEM3'); ?>"><p class="text-3xl py-8">REEDEM</p></a>
+        </div>
+    </div>
+</div>
+</div>
+</div>
